4c0ca4d1403bac64509f594a5312218cf9e19798
Samuel <sp5.95@libero.it>
2022-10-12 19:54:24 +0200

    prova

--- pages/index.js
+++ pages/index.js
@@ -27,23 +27,28 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about COMING SOON features and API.</p>
           </a>
 
           <a href="" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>
+              Learn about COOMING SOON in an interactive course with quizzes!
+            </p>
           </a>
 
           <a href="" className={styles.card}>
             <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>
+              Discover and deploy boilerplate example COOMING SOON projects.
+            </p>
           </a>
 
           <a href="" className={styles.card}>
             <h2>Deploy &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your COOMING SOON site to a public URL with
+              Vercel.
             </p>
           </a>
         </div>
