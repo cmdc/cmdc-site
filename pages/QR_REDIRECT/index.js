ba6d80e83015674f5c8c2f303a70f57d4f7a37d5
Samuel <sp5.95@libero.it>
2022-10-17 18:24:47 +0200

    add qr redirect and mongo

--- pages/QR_REDIRECT/index.js
+++ pages/QR_REDIRECT/index.js
@@ -1,35 +1,72 @@
 import React from "react";
 import Seo from "../../components/common/seo";
+import clientPromise from "../../lib/mongodb";
 
-const QR = () => {
-  // COUNT SCAN
-  // UPDATE COLLECTIONS DB
-  // REDIRECT
+const QR = ({ count, lasts }) => {
   return (
     <>
       <Seo />
-      <>QR</>
+      <div style={{ marginTop: "6rem", marginLeft: "2rem" }}>
+        <p>COUNT: {count}</p>
+        <br />
+        <p>LAST:</p>
+        {lasts.length > 0 &&
+          lasts.map((item, index) => {
+            return (
+              <div key={index}>
+                Timestamp: {item?.timestamp}, Platform: {item?.platform}, Ip:{" "}
+                {item?.ip}, UserAgent: {item.userAgent}
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
 
-QR.getInitialProps = async ({ res }) => {
-  const targetURL = "/";
-  if (res) {
-    res.writeHead(307, { Location: targetURL });
-    res.end();
-    /* await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(res.writeHead(307, { Location: targetURL }) && res.end());
-      }, 2000);
-    }); */
+export async function getServerSideProps({ req, query }) {
+  const ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
+  const userAgent = req.headers["user-agent"];
+  const platform = req.headers["sec-ch-ua-platform"];
+
+  const client = await clientPromise;
+  const db = client.db("cmdc");
+  const collection = db.collection("qr-scan");
+
+  // MODE ADMIN
+  if (query?.mode === "admin") {
+    // COUNT SCAN
+    const count = await collection.count();
+    // LAST 20
+    const lasts = await collection
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(20)
+      .toArray();
+
+    return {
+      props: {
+        count: JSON.parse(JSON.stringify(count)),
+        lasts: JSON.parse(JSON.stringify(lasts)),
+      },
+    };
   } else {
-    window.location = targetURL;
+    // UPDATE COLLECTIONS DB
+    await collection.insertOne({
+      timestamp: new Date(),
+      platform: platform,
+      userAgent: userAgent,
+      ip: ip,
+    });
 
-    await new Promise((resolve) => {});
+    // REDIRECT
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
   }
-
-  return {};
-};
+}
 
 export default QR;
